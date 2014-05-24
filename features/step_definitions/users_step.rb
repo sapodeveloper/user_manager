41031dd7ed70be082b3150b25f90e32a6f0701dd
morishita <tomohiro.m0219@gmail.com>
2014-05-24 18:18:51 +0900

    #3 ユーザ操作に関するテスト作成

---
+++ features/step_definitions/users_step.rb
@@ -0,0 +1,59 @@
+前提(/^システム管理者がログインしている$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^ユーザ一覧ページにアクセスする$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^ユーザ一覧を表示する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^ユーザ検索ページにアクセスする$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^検索フォームに検索条件を入力する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^検索結果を表示する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^ユーザ新規作成ページにアクセスする$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^登録フォームに情報を入力する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^登録ユーザ情報を表示する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^登録フォームに誤った情報を入力する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^ユーザ情報変更ページにアクセスする$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^更新したユーザ情報を表示する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^ユーザシステム権限を変更する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^ユーザ状態を変更する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^凍結したユーザ情報を表示する$/) do
+  pending # express the regexp above with the code you wish you had
+end
