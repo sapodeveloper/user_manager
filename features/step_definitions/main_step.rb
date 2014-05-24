afe96c02556abfc860525b8eb46bebd52370e4f4
morishita <tomohiro.m0219@gmail.com>
2014-05-24 18:07:50 +0900

    #3 メインページに関するテスト作成

---
+++ features/step_definitions/main_step.rb
@@ -0,0 +1,11 @@
+もし(/^メインページにアクセスする$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^メインページを表示する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^ログインユーザ情報を表示する$/) do
+  pending # express the regexp above with the code you wish you had
+end
