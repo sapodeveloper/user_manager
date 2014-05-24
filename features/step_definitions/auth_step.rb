da6158a89fefd6fd245b0587b2dea9c5e803f4c7
morishita <tomohiro.m0219@gmail.com>
2014-05-24 18:04:04 +0900

    #3 認証に関するテスト作成

---
+++ features/step_definitions/auth_step.rb
@@ -0,0 +1,43 @@
+前提(/^ユーザが存在している$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^ログインページにアクセスする$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^ログインフォームを表示する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^各フォームに入力する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^"(.*?)"ボタンを押す$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^ログイン後のページを表示する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^各フォームに誤った入力する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^"(.*?)"と表示する$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+前提(/^ログインしている$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^"(.*?)"ボタンを押す$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^ログインページを表示する$/) do
+  pending # express the regexp above with the code you wish you had
+end
